f3fd05e106b19c222631290b6f9b70d6a10128ff
kosiedoziem <edoziemsomto@gmail.com>
2025-12-20 21:20:03 -0600

    saved changes

--- src/Card.java
+++ src/Card.java
@@ -1,4 +1,9 @@
 public class Card {
     ColorEnum color;
-    
+    ValueEnum value;
+
+    public Card(ColorEnum color, ValueEnum value) {
+        this.color = color;
+        this.value = value;
+    }
 }
